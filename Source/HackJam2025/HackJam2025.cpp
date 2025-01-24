9fb9c32b14d6161b3f2da46ff5b07c975d75de10
Krzysztof Włódarczak <krzysztofprzem@gmail.com>
2025-01-24 17:27:02 +0100

    Init commit

---
+++ Source/HackJam2025/HackJam2025.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HackJam2025.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HackJam2025, "HackJam2025" );
+ 
---
+++ Source/HackJam2025/HackJam2025GameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HackJam2025GameMode.generated.h"
+
+UCLASS(minimalapi)
+class AHackJam2025GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AHackJam2025GameMode();
+};
+
+
+
